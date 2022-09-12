82516f72658f6078d334b0c4f8fb69a8f2ec2523
Gerald Horn <geraldmhorn@gmail.com>
2022-09-11 21:41:30 -0700

    Test discrete lookup

--- pkg/spectrum/discrete_test.go
+++ pkg/spectrum/discrete_test.go
@@ -12,10 +12,43 @@ func TestDiscrete_Lookup(t *testing.T) {
 		d[idx] = float64(idx)
 	}
 
-	assert.Equal(t, 0.0, d.Lookup(200))
-	assert.Equal(t, 0.0, d.Lookup(400))
-	assert.Equal(t, 1.0, d.Lookup(405))
-	assert.Equal(t, 1.0, d.Lookup(406))
-	assert.Equal(t, 59.0, d.Lookup(700))
-	assert.Equal(t, 59.0, d.Lookup(900))
+	tests := []struct {
+		name       string
+		wavelength int
+		expected   int
+	}{{
+		name:       "below min",
+		wavelength: discWavelengthMin - 1,
+		expected:   0,
+	}, {
+		name:       "at min",
+		wavelength: discWavelengthMin,
+		expected:   0,
+	}, {
+		name:       "above min",
+		wavelength: discWavelengthMin + 1,
+		expected:   0,
+	}, {
+		name:       "at next step",
+		wavelength: discWavelengthMin + discWavelengthStep,
+		expected:   1,
+	}, {
+		name:       "above next step",
+		wavelength: discWavelengthMin + discWavelengthStep + 1,
+		expected:   1,
+	}, {
+		name:       "at max",
+		wavelength: discWavelengthMax,
+		expected:   DiscreteSize - 1,
+	}, {
+		name:       "above max",
+		wavelength: discWavelengthMax + 1,
+		expected:   DiscreteSize - 1,
+	}}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equal(t, float64(tt.expected), d.Lookup(float64(tt.wavelength)))
+		})
+	}
 }
