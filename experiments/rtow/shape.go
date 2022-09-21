72f99de9f8b2a2f6e5b47a7ca5424bf2202320af
Gerald Horn <geraldmhorn@gmail.com>
2022-09-21 12:10:40 -0700

    Update RTOW

--- experiments/rtow/shape.go
+++ experiments/rtow/shape.go
@@ -11,7 +11,7 @@ import (
 // inside the object.
 type Hit struct {
 	Pt       geo.Vec
-	Norm     geo.Unit
+	Norm     geo.Vec
 	T        float64
 	Interior bool
 }
@@ -46,7 +46,7 @@ type Sphere struct {
 
 func (s *Sphere) Hit(r *geo.Ray, tMin, tMax float64) (*Hit, bool) {
 	oc := r.Origin.Minus(s.Center)
-	a := 1.0 // r.direction().length_squared()
+	a := r.Dir.Dot(r.Dir) // r.Dir.length_squared()
 	halfB := oc.Dot(geo.Vec(r.Dir))
 	c := oc.Dot(oc) - s.Radius*s.Radius
 
@@ -67,7 +67,7 @@ func (s *Sphere) Hit(r *geo.Ray, tMin, tMax float64) (*Hit, bool) {
 
 	// Calculate the intersection point and outward normal.
 	point := r.At(root)
-	norm, _ := point.Minus(s.Center).Unit()
+	norm := point.Minus(s.Center)
 
 	// Determine if ray is coming from inside the sphere
 	interior := r.Dir.Enters(norm)
