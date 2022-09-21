dd55229fa9a02fe7f8efc280c7c49ee74d0cac25
Gerald Horn <geraldmhorn@gmail.com>
2022-09-21 10:42:32 -0700

    Add test for matrix transpose

--- pkg/geo/matrix_test.go
+++ pkg/geo/matrix_test.go
@@ -18,15 +18,29 @@ func TestMatrixMult(t *testing.T) {
 		{12, 1, 4, 9},
 		{9, 10, 12, 2},
 		{3, 12, 4, 10}}
-	assert.Equal(t, Matrix{
+	assert.Equal(t, &Matrix{
 		{210, 267, 236, 271},
 		{93, 149, 104, 149},
 		{171, 146, 172, 268},
 		{105, 169, 128, 169},
-	}, *(a.Mult(b)))
+	}, a.Mult(b))
 }
 
-func TestInv(t *testing.T) {
+func TestMatrixTranspose(t *testing.T) {
+	m := Matrix{
+		{10, 11, 12, 13},
+		{14, 15, 16, 17},
+		{18, 19, 20, 21},
+		{22, 23, 24, 25}}
+
+	assert.Equal(t, &Matrix{
+		{10, 14, 18, 22},
+		{11, 15, 19, 23},
+		{12, 16, 20, 24},
+		{13, 17, 21, 25}}, m.T())
+}
+
+func TestMatrixInv(t *testing.T) {
 	a := &Matrix{
 		{3, 4, 6, 8},
 		{1, 2, 7, 2},
