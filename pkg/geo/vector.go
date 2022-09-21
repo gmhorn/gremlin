eb5697cd5f7c24d920995f6bfabe9b64c39ebd61
Gerald Horn <geraldmhorn@gmail.com>
2022-09-21 11:41:20 -0700

    Clean up vector doc

--- pkg/geo/vector.go
+++ pkg/geo/vector.go
@@ -18,27 +18,32 @@ var Origin = Vec{0, 0, 0}
 // implementation code.
 type Vec [3]float64
 
-// Plus returns the Vec a + b.
+// Plus returns the vector a + b.
 func (a Vec) Plus(b Vec) Vec {
 	return Vec{a[0] + b[0], a[1] + b[1], a[2] + b[2]}
 }
 
-// Minus returns the Vec a - b.
+// Minus returns the vector a - b.
 func (a Vec) Minus(b Vec) Vec {
 	return Vec{a[0] - b[0], a[1] - b[1], a[2] - b[2]}
 }
 
-// Scale returns a copy of this Vec, scaled by t.
+// Scale returns a copy of this vector scaled by t.
 func (a Vec) Scale(t float64) Vec {
 	return Vec{t * a[0], t * a[1], t * a[2]}
 }
 
-// Dot returns the dot product of this Vec with b.
+// Reverse returns a copy of this vector with all components flipped.
+func (a Vec) Reverse() Vec {
+	return Vec{-a[0], -a[1], -a[2]}
+}
+
+// Dot returns the dot product of this vector with b.
 func (a Vec) Dot(b Vec) float64 {
 	return a[0]*b[0] + a[1]*b[1] + a[2]*b[2]
 }
 
-// Cross returns the cross product of this Vec with b.
+// Cross returns the cross product of this vector with b.
 func (a Vec) Cross(b Vec) Vec {
 	return Vec{
 		a[1]*b[2] - a[2]*b[1],
@@ -47,24 +52,24 @@ func (a Vec) Cross(b Vec) Vec {
 	}
 }
 
-// Unit normalizes this Vec.
+// Unit normalizes this vector.
 func (a Vec) Unit() (Unit, bool) {
 	d := a.Len()
 	return Unit(a.Scale(1 / d)), d > 0
 }
 
-// Len returns the length of this Vec.
+// Len returns the length of this vector.
 func (a Vec) Len() float64 {
 	return math.Sqrt(a[0]*a[0] + a[1]*a[1] + a[2]*a[2])
 }
 
-// HasNaNs returns true if any component of this Vec is an IEEE 754
+// HasNaNs returns true if any component of this vector is an IEEE 754
 // NaN.
 func (a Vec) HasNaNs() bool {
 	return math.IsNaN(a[0]) || math.IsNaN(a[1]) || math.IsNaN(a[2])
 }
 
-// String returns a string representation of this Vec.
+// String returns a string representation of this vector.
 func (a *Vec) String() string {
 	if a == nil {
 		return ""
