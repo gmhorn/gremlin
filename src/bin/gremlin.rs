3a7031a586c17369f486906fc8708ee6913ed440
Gerald Horn <geraldmhorn@gmail.com>
2022-10-16 22:32:54 -0700

    Add some operators for stuff

--- src/bin/gremlin.rs
+++ src/bin/gremlin.rs
@@ -3,8 +3,13 @@ use gremlin::geo::{Vec3, Point3};
 fn main() {
     println!("Hello from gremlin!");
 
-    let v = Vec3::splat(2.0);
-    let p = Point3::splat(12.0);
+    let p1 = Point3::splat(1.0);
+    let p2 = Point3::new(5.0, 6.0, 7.0);
 
-    println!("Got a vector {:?} and point {:?}", v, p);
+    let v1 = Vec3::new(-1.0, -2.0, -3.0);
+    let point_plus_point = p1 + p2;
+    let point_plus_vec = p1 + v1;
+
+    println!("{:?} + {:?} = {:?}", p1, p2, point_plus_point);
+    println!("{:?} + {:?} = {:?}", p1, v1, point_plus_vec);
 }
