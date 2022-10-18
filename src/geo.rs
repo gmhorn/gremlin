baaaefe774df7da643c2616d8e59a94eab149b48
Gerald Horn <geraldmhorn@gmail.com>
2022-10-17 23:55:15 -0700

    Start adding docs and tests

--- src/geo.rs
+++ src/geo.rs
@@ -1,3 +1,15 @@
+//! # Geometry module
+//! 
+//! Implements basic geometric primitives needed for ray tracing. Attempts to
+//! be simple, minimal, and fast.
+//! 
+//! * Uses minimal generics (`f64` is sufficient most of the time)
+//! * Directly implements operator traits rather than relying on macros
+//! * Supports the minimum subset of functionality needed by other modules
+//! 
+//! It does **not** attempt to be a fully-featured euclidean geometry library,
+//! nor a fully-featured linear algebra library.
+
 mod matrix;
 pub use self::matrix::*;
 
