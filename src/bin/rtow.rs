0e0240a45482a9be1d20ccfafed5683f1ccf11a1
Gerald Horn <geraldmhorn@gmail.com>
2022-10-22 10:06:11 -0700

    Create initial png

--- src/bin/rtow.rs
+++ src/bin/rtow.rs
@@ -1,5 +1,21 @@
-use gremlin::geo;
+use image::{RgbImage};
 
 fn main() {
-    let _a = geo::point(1.0, 1.0, 1.0);
+    let imgx = 800;
+    let imgy = 600;
+
+    let scalex = (imgx as f32) - 1.0;
+    let scaley = (imgy as f32) - 1.0;
+
+    let mut img = RgbImage::new(imgx, imgy);
+    
+    for (x, y, pixel) in img.enumerate_pixels_mut() {
+        let r = ((x as f32) / scalex) * 255.0;
+        let g = ((y as f32) / scaley) * 255.0;
+        let b = 0.25 * 255.0;
+
+        *pixel = image::Rgb([r as u8, g as u8, b as u8]);
+    }
+
+    img.save("rtow.png").unwrap();
 }
