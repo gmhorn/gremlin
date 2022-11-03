bd66714ab9ee323834c92c8f2bb57c411f43b581
Gerald Horn <geraldmhorn@gmail.com>
2022-11-02 21:30:31 -0700

    Get rid of num traits remains

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 //! # Gremlin
 //!
 //! Gremlin is a ray tracer
-
-use std::ops::{AddAssign, MulAssign, SubAssign};
-
-use num_traits::Float;
-
 pub mod film;
 pub mod geo;
 pub mod spectrum;
@@ -24,16 +19,3 @@ pub use std::f32 as floats;
 pub type MyFloat = f64;
 #[cfg(not(feature = "f32"))]
 pub use std::f64 as floats;
-
-/// Trait defining "real-valued" numbers.
-///
-/// Allows us to be generic over both `f32` and `f64` primitive types.
-pub trait Real: Float + AddAssign + SubAssign + MulAssign + From<f32> {
-    #[inline(always)]
-    fn from_f32(val: f32) -> Self {
-        val.into()
-    }
-}
-
-impl Real for f32 {}
-impl Real for f64 {}
