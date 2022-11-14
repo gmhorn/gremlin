ab7cb4f3d3d14a17bee2d55c944088ec2057bee1
Gerald Horn <geraldmhorn@gmail.com>
2022-11-13 16:34:43 -0800

    Update bench

--- benches/shape.rs
+++ benches/shape.rs
@@ -1,40 +1,42 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use gremlin::{geo::Point, shape::*};
-use rand::prelude::*;
+use gremlin::{
+    geo::{Point, Ray, Vector},
+    shape::*,
+    Float,
+};
+
+pub fn direct_dispatch(c: &mut Criterion) {
+    let sphere = Sphere::new(Point::new(10.0, 0.0, 0.0), 1.0);
+    let ray = Ray::new(Point::ORIGIN, Vector::X_AXIS);
+
+    c.bench_function("direct dispatch", |b| {
+        b.iter(|| {
+            let _ = black_box(sphere.intersect(&ray, 0.0, Float::INFINITY));
+        })
+    });
+}
 
 pub fn enum_dispatch(c: &mut Criterion) {
-    let mut rng = rand::thread_rng();
-    let mut shapes: Vec<Surface> = vec![];
-    for _ in 0..100000 {
-        if rng.gen() {
-            shapes.push(Surface::from(Sphere::new(Point::ORIGIN, 1.0)));
-        } else {
-            shapes.push(Surface::from(Triangle));
-        }
-    }
+    let surf = Surface::from(Sphere::new(Point::new(10.0, 0.0, 0.0), 1.0));
+    let ray = Ray::new(Point::ORIGIN, Vector::X_AXIS);
+
     c.bench_function("enum dispatch", |b| {
         b.iter(|| {
-            let _: Vec<_> = black_box(shapes.iter().map(|s| s.value()).collect());
+            let _ = black_box(surf.intersect(&ray, 0.0, Float::INFINITY));
         })
     });
 }
 
 pub fn dynamic_dispatch(c: &mut Criterion) {
-    let mut rng = rand::thread_rng();
-    let mut shapes: Vec<Box<dyn Shape>> = vec![];
-    for _ in 0..100000 {
-        if rng.gen() {
-            shapes.push(Box::new(Sphere::new(Point::ORIGIN, 1.0)));
-        } else {
-            shapes.push(Box::new(Triangle));
-        }
-    }
+    let shape: Box<dyn Shape> = Box::new(Sphere::new(Point::new(10.0, 0.0, 0.0), 1.0));
+    let ray = Ray::new(Point::ORIGIN, Vector::X_AXIS);
+
     c.bench_function("dynamic dispatch", |b| {
         b.iter(|| {
-            let _: Vec<_> = black_box(shapes.iter().map(|s| s.value()).collect());
+            let _ = black_box(shape.intersect(&ray, 0.0, Float::INFINITY));
         })
     });
 }
 
-criterion_group!(shape, enum_dispatch, dynamic_dispatch,);
+criterion_group!(shape, direct_dispatch, enum_dispatch, dynamic_dispatch,);
 criterion_main!(shape);
